86293c5fb4d8bad29350ea17b23abc39e8d9088d
pligin <pligin@PSWeb>
2019-08-07 21:00:38 +0300

    Небольшие правки

--- engine/inc/billing.php
+++ engine/inc/billing.php
@@ -1,13 +1,11 @@
 <?php
-/*
-=====================================================
-Payeer for DLEBilling
------------------------------------------------------
-pligin
------------------------------------------------------
- This code is copyrighted
-=====================================================
-*/
+/**
+ * DLE Billing
+ *
+ * @link          https://github.com/iPSWeb/DLE-Billing
+ * @author        PSWeb.ru <i@psweb.ru>
+ * @copyright     Copyright (c) 2019, pligin
+ */
 
 if(!defined('DATALIFEENGINE')) {
   die("Hacking attempt!");
